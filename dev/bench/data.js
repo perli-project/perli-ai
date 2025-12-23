ab9e0364675e5a22cda10ce089688a61c8770a2c
github-action-benchmark <github@users.noreply.github.com>
2025-12-23 09:43:28 +0000

    add Benchmark (jmh) benchmark result for daa83abe1c4609a15964a2aea960f1a6f58c460b

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1766483008515,
+  "repoUrl": "https://github.com/perli-project/perli-ai",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "81628455+WhiteBin-bin@users.noreply.github.com",
+            "name": "Baek HyeonBin",
+            "username": "WhiteBin-bin"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "daa83abe1c4609a15964a2aea960f1a6f58c460b",
+          "message": "🐳 Devops: CI 파이프라인 구축 (#16)",
+          "timestamp": "2025-12-23T18:35:41+09:00",
+          "tree_id": "6711bf044c0ea7b4e7fb69082f3ab6cdd5e51bc3",
+          "url": "https://github.com/perli-project/perli-ai/commit/daa83abe1c4609a15964a2aea960f1a6f58c460b"
+        },
+        "date": 1766483007875,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "aicard.perli.ml.benchmark.UpliftBenchmark.benchmarkPredictUplift",
+            "value": 6.320404205219468e-7,
+            "unit": "ms/op",
+            "extra": "iterations: 1\nforks: 1\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
